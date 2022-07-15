a6da9a8b2945b55d73d8bc61a48712fa80ea34ba
Vladick <cool.valad12@gmail.com>
2022-07-16 02:28:30 +0300

    init commit

---
+++ src/main/java/com/ispirer/IspirerTest/IspirerTestApplication.java
@@ -0,0 +1,13 @@
+package com.ispirer.IspirerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspirerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IspirerTestApplication.class, args);
+	}
+
+}
